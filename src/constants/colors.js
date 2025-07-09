7d7a1ad78d4ac867b72fbd729ed4d4a7dccdf6d7
Jordan <42895790+PsychicRoot@users.noreply.github.com>
2025-07-09 16:26:10 +0200

    Create colors.js

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+module.exports = {
+    COLOR_ATLAS_LEGACY: 0x3196df,
+    COLOR_ATLAS_ENHANCED: 0x113752,
+    COLOR_DISCORD: 0x7289da,
+    COLOR_CS2: 0xfbac1c,
+    COLOR_LAUNCHER: 0x000000,
+    COLOR_WEBSITE: 0x36393e,
+    COLOR_KOFI: 0xff5a16,
+  };
+  
